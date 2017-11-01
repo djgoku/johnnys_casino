fcf8dd63efe1c8d56a8bb514c0e43ab5189f314b
Jonathan C. Otsuka <djgoku@gmail.com>
2017-10-31 22:26:08 -0500

    Start the implementation of Dealer. Add the ability to have a player join the dealer. Add tests to make sure the dealer behaves.

--- apps/dealer/lib/dealer.ex
+++ apps/dealer/lib/dealer.ex
@@ -1,18 +1,62 @@
 defmodule Dealer do
   @moduledoc """
-  Documentation for Dealer.
+  A dealer controls all aspects of a table at johnny's casino.
   """
 
+  use GenServer
+  require Logger
+
+
+  # Client
+
+  def start_link([]) do
+    GenServer.start_link(__MODULE__, [], [name: __MODULE__])
+  end
+
+  def init([]) do
+    {:ok, %{players: [], max_players: 7}}
+  end
+
   @doc """
-  Hello world.
+  Join a casino table.
 
   ## Examples
 
-      iex> Dealer.hello
-      :world
+      # Number returned is position at table
+      iex> Dealer.join()
+      {:player, 1}
 
+      # error trying to re-join table
+      iex> Dealer.join()
+      {:error, :already_joined_table}
+
+      # error max players at table
+      iex> Dealer.join()
+      {:error, :max_players_at_table}
   """
-  def hello do
-    :world
+  @spec join() :: {:ok, {:player_number, pos_integer()}} | {:error, any()}
+  def join() do
+    GenServer.call(__MODULE__, :join)
+  end
+
+  # Server
+
+  def handle_call(:join, {pid, _term}, state) do
+    Logger.debug("(Dealer) received join from #{inspect pid}")
+
+    max_players = state[:max_players]
+    current_players = state[:players]
+
+    case length(current_players) do
+      x when x < max_players ->
+        if Enum.member?(current_players, pid) do
+          {:reply, {:error, :already_joined_table}, state}
+        else
+          all_players = current_players ++ [pid]
+          {:reply, {:player, length(all_players)}, %{state | players: all_players}}
+        end
+      _ ->
+        {:reply, {:error, :max_players_at_table}, state}
+    end
   end
 end
--- apps/dealer/test/dealer_test.exs
+++ apps/dealer/test/dealer_test.exs
@@ -2,7 +2,31 @@ defmodule DealerTest do
   use ExUnit.Case
   doctest Dealer
 
-  test "greets the world" do
-    assert Dealer.hello() == :world
+  test "player can join table" do
+    state = %{players: [], max_players: 7}
+    {:reply, reply, new_state} = Dealer.handle_call(:join, {:pid, :term}, state)
+
+    assert reply == {:player, 1}
+    assert new_state[:players] == [:pid]
+  end
+
+  test "player gets error when trying to rejoin table" do
+    state = %{players: [], max_players: 7}
+    {:reply, reply, new_state} = Dealer.handle_call(:join, {:pid, :term}, state)
+
+    assert reply == {:player, 1}
+    assert new_state[:players] == [:pid]
+
+    {:reply, reply, new_state} = Dealer.handle_call(:join, {:pid, :term}, new_state)
+
+    assert reply == {:error, :already_joined_table}
+  end
+
+  test "player is unable to join a full table" do
+    state = %{players: [:not_test_pid], max_players: 1}
+    {:reply, reply, new_state} = Dealer.handle_call(:join, {:pid, :term}, state)
+
+    assert reply == {:error, :max_players_at_table}
+    assert new_state[:players] == [:not_test_pid]
   end
 end
