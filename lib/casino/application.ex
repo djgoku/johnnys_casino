d44dd8099c8f53d567e9c7f7287b08de56d09847
Jonathan C. Otsuka <djgoku@gmail.com>
2018-01-29 23:23:47 -0600

    Add ExCardDeck to supervisor tree.

--- lib/casino/application.ex
+++ lib/casino/application.ex
@@ -8,7 +8,8 @@ defmodule Casino.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {Casino.Dealer, []}
+      {Casino.Dealer, []},
+      {ExCardDeck, 1}
       # Starts a worker by calling: Casino.Worker.start_link(arg)
       # {Casino.Worker, arg},
     ]
