8db185bbf1d6361c9431430ce17b11a8c499877b
Jonathan C. Otsuka <djgoku@gmail.com>
2018-02-26 21:54:55 -0600

    add ex_card_deck dependency.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Casino.Mixfile do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_card_deck, git: "https://github.com/djgoku/ex_card_deck.git"},
     ]
   end
 end
