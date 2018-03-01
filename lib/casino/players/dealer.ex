383d0fb72c5266d822183721b6568ddad8952dcd
Jonathan C. Otsuka <djgoku@gmail.com>
2018-02-28 21:40:53 -0600

    add a handler to reset the current hand after a new game has been started.

--- lib/casino/players/dealer.ex
+++ lib/casino/players/dealer.ex
@@ -43,6 +43,12 @@ defmodule Casino.Player.Dealer do
     {:reply, hit_or_stay, state}
   end
 
+  def handle_info(:new_game, state) do
+    
+    state = %{state | current_hand: []}
+    {:noreply, state}
+  end
+
   def handle_info({:card, card}, state) do
     Logger.info("(#{__MODULE__}) we were dealt a #{inspect(card)}")
     current_hand = state[:current_hand]
--- lib/casino/players/just_stay.ex
+++ lib/casino/players/just_stay.ex
@@ -29,6 +29,12 @@ defmodule Casino.Player.JustStay do
     {:reply, :stay, state}
   end
 
+  def handle_info(:new_game, state) do
+    
+    state = %{state | current_hand: []}
+    {:noreply, state}
+  end
+
   def handle_info({:card, card}, state) do
     Logger.info("(#{__MODULE__}) we were dealt a #{inspect(card)}")
     current_hand = state[:current_hand]
